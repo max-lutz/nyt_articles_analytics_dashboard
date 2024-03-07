2948ce3cb7a843d749c844ecd94fbddecc1049b5
max-lutz <max.lutz.09@gmail.com>
2024-03-07 17:41:13 +0000

    create export functions to gcs and bigquery

---
+++ nyt-etl-pipeline/data_exporters/transformed_data_to_gcs_partitioned.py
@@ -0,0 +1,28 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/personal-gcp.json'
+
+bucket_name = 'nyt_data_analytics_bucket'
+project_id = 'nyt-data-analytics'
+table_name ='nyt_raw_data'
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols = ['pub_date'],
+        filesystem = gcs
+    )
